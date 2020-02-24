ff94f04a8a76aa5308eb471fad1a1f5df4f5c6f6
Farhad AzadEhyaei <me.ehyaie@gmail.com>
2020-02-24 13:19:28 +0330

    refactor

--- app/src/main/java/com/example/soroushprofile/models/ConversationFactory.kt
+++ app/src/main/java/com/example/soroushprofile/models/ConversationFactory.kt
@@ -1,7 +1,6 @@
 package com.example.soroushprofile.models
 
 import com.example.soroushprofile.R
-import java.util.*
 
 object ConversationFactory {
 
@@ -11,26 +10,25 @@ object ConversationFactory {
                 val bio = "27 y.o. designer from tehran, iran..., The Biotechnology Innovation Organization" +
                         " is the largest trade organization in the world that represents the biotechnology industry."
                 val user = User("Farhad Azad", R.drawable.header, bio)
-                val media = object : ArrayList<String>() {
-                    init {
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-2-634x467.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-3.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-5-634x475.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-101-634x475.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-14-634x396.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-16-634x410.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-19-634x396.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-2-634x467.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-3.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-5-634x475.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-101-634x475.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-14-634x396.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-16-634x410.jpg")
-                        add("https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-19-634x396.jpg")
-                    }
-                }
+                val media = arrayListOf(
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-2-634x467.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-3.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-5-634x475.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-101-634x475.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-14-634x396.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-16-634x410.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-19-634x396.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-2-634x467.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-3.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-5-634x475.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-101-634x475.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-14-634x396.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-16-634x410.jpg",
+                        "https://baelm.net/wp-content/uploads/2014/11/Nature-Photo-19-634x396.jpg"
+                )
                 return IndividualConversation(media, user)
             }
+
             ConversationType.Group -> return GroupConversation(null, null, "Soroush", 231)
             ConversationType.Channel -> return ChannelConversation(null, "khabar 24 is live..", "Khabar24", 429)
             else -> throw IllegalArgumentException("not supported type.")
